4fbfc2909977ce19f56b9037a0032412cdd53ccd
serxka <serxka@protonmail.com>
2021-11-29 15:51:41 +1100

    Fixed up static mut

--- lib/macros.rs
+++ lib/macros.rs
@@ -7,28 +7,38 @@ macro_rules! robot {
 		#[no_mangle]
 		extern "C" fn initialize() {
 			$crate::rtos::spawn(|| {
-				ROBOT.call_once(|| $robot::new());
+				unsafe {
+					ROBOT.call_once(|| $robot::new());
+				}
 			});
 		}
 
 		#[no_mangle]
 		extern "C" fn disabled() {
-			ROBOT.wait().disabled()
+			unsafe {
+				ROBOT.wait().disabled()
+			}
 		}
 
 		#[no_mangle]
 		extern "C" fn competition_initialize() {
-			ROBOT.wait().competition_init()
+			unsafe {
+				ROBOT.wait().competition_init()	
+			}
 		}
 
 		#[no_mangle]
 		extern "C" fn autonomous() {
-			ROBOT.wait().autonomous()
+			unsafe {
+				ROBOT.wait().autonomous()
+			}
 		}
 
 		#[no_mangle]
 		extern "C" fn opcontrol() {
-			ROBOT.wait().opcontrol()
+			unsafe {
+				ROBOT.wait().opcontrol()
+			}
 		}
 	};
 }
