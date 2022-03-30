f350b1367181193082007b49975808620ffe0d46
serxka <serxka@protonmail.com>
2022-03-30 19:51:30 +1100

    Turned ports in arrays

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -11,6 +11,8 @@ pub mod imu;
 pub mod motor;
 pub mod rotation;
 
+use smallvec::SmallVec;
+
 use crate::util::get_errno;
 
 /// Possible errors that could be returned from devices in their operation.
@@ -89,35 +91,8 @@ pub struct Devices {
 	pub master_controller: Option<Controller>,
 	/// Secondary Controller
 	pub slave_controller: Option<Controller>,
-	pub port01: Option<Port>,
-	pub port02: Option<Port>,
-	pub port03: Option<Port>,
-	pub port04: Option<Port>,
-	pub port05: Option<Port>,
-	pub port06: Option<Port>,
-	pub port07: Option<Port>,
-	pub port08: Option<Port>,
-	pub port09: Option<Port>,
-	pub port10: Option<Port>,
-	pub port11: Option<Port>,
-	pub port12: Option<Port>,
-	pub port13: Option<Port>,
-	pub port14: Option<Port>,
-	pub port15: Option<Port>,
-	pub port16: Option<Port>,
-	pub port17: Option<Port>,
-	pub port18: Option<Port>,
-	pub port19: Option<Port>,
-	pub port20: Option<Port>,
-	pub port21: Option<Port>,
-	pub port_a: Option<TriPort>,
-	pub port_b: Option<TriPort>,
-	pub port_c: Option<TriPort>,
-	pub port_d: Option<TriPort>,
-	pub port_e: Option<TriPort>,
-	pub port_f: Option<TriPort>,
-	pub port_g: Option<TriPort>,
-	pub port_h: Option<TriPort>,
+	pub ports: [Option<Port>; 21],
+	pub triports: [Option<TriPort>; 8],
 }
 
 impl Devices {
@@ -132,38 +107,21 @@ impl Devices {
 	/// sensors, etc. This would cause panics or undefined behaviour to occur in
 	/// seemingly innocuous code.
 	pub unsafe fn new() -> Self {
+		let mut ports: SmallVec<[_; 21]> = SmallVec::new();
+		for i in 1..22 {
+			ports.push(Some(Port::new(i)));
+		}
+
+		let mut triports: SmallVec<[_; 8]> = SmallVec::new();
+		for i in 1..9 {
+			triports.push(Some(TriPort::new(i, None)));
+		}
+
 		Devices {
 			master_controller: Some(Controller::master()),
 			slave_controller: Some(Controller::slave()),
-			port01: Some(Port::new(1)),
-			port02: Some(Port::new(2)),
-			port03: Some(Port::new(3)),
-			port04: Some(Port::new(4)),
-			port05: Some(Port::new(5)),
-			port06: Some(Port::new(6)),
-			port07: Some(Port::new(7)),
-			port08: Some(Port::new(8)),
-			port09: Some(Port::new(9)),
-			port10: Some(Port::new(10)),
-			port11: Some(Port::new(11)),
-			port12: Some(Port::new(12)),
-			port13: Some(Port::new(13)),
-			port14: Some(Port::new(14)),
-			port15: Some(Port::new(15)),
-			port16: Some(Port::new(16)),
-			port17: Some(Port::new(17)),
-			port18: Some(Port::new(18)),
-			port19: Some(Port::new(19)),
-			port20: Some(Port::new(20)),
-			port21: Some(Port::new(21)),
-			port_a: Some(TriPort::new(1, None)),
-			port_b: Some(TriPort::new(2, None)),
-			port_c: Some(TriPort::new(3, None)),
-			port_d: Some(TriPort::new(4, None)),
-			port_e: Some(TriPort::new(5, None)),
-			port_f: Some(TriPort::new(6, None)),
-			port_g: Some(TriPort::new(7, None)),
-			port_h: Some(TriPort::new(8, None)),
+			ports: ports.into_inner().unwrap(),
+			triports: triports.into_inner().unwrap(),
 		}
 	}
 }
