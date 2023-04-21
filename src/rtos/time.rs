4eb38b2fdc8e9d94cc95d715230339f2dafeffdb
Milo <40355097+serxka@users.noreply.github.com>
2023-04-22 08:53:13 +1000

    Update time.rs

--- src/rtos/time.rs
+++ src/rtos/time.rs
@@ -115,7 +115,7 @@ impl Instant {
 	/// panic will occur if `self` measures a time in the future that has not
 	/// yet occurred.
 	pub fn elapsed(&self) -> Duration {
-		Duration::from_micros((Instant::now() - *self).as_millis())
+	 (Instant::now() - *self).as_duration()
 	}
 
 	/// Return the duration between this `Instant` and an earlier `Instant`.
